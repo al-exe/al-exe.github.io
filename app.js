d0ba7568371e0bb6fde92fee8e7b4281b5ff516e
Alex Lee <al.exe@berkeley.edu>
2023-03-16 18:47:58 -0700

    example test

---
+++ app.js
@@ -0,0 +1,14 @@
+var Box = React.createClass({
+   render: function () {
+       return (
+           
+           <div>
+               <h1>React</h1> 
+               <h1>React2</h1>
+           </div>
+           
+       );
+   } 
+});
+
+ReactDOM.render(<Box />, document.getElementById('app'));

f8767e178f43b55716d9c35e1fdef6c72943c76a
Alex Lee <al.exe@berkeley.edu>
2023-03-16 18:59:19 -0700

    commit

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 var Box = React.createClass({
-   render: function () {
-       return (
-           
-           <div>
-               <h1>React</h1> 
-               <h1>React2</h1>
-           </div>
-           
-       );
-   } 
+  render: function () {
+      return (
+          <div>
+              <h1>React</h1> 
+              <h1>React2</h1>
+          </div>
+          
+      );
+  } 
 });
 
 ReactDOM.render(<Box />, document.getElementById('app'));
